d452e6782a1452d751451568b472cba16547da4f
Amogh Singhal <devamoghs@gmail.com>
2019-04-21 18:07:57 +0530

    Update first_example.py

--- first_example.py
+++ first_example.py
@@ -6,7 +6,7 @@ coefficients = np.array([[1], [-20], [25]])
 w = tf.Variable([0], dtype=tf.float32)
 x = tf.placeholder(tf.float32, [3,1])
 
-cost = x[0][0]*w**2 + x[0][1]*w + x[0][2]
+cost = x[0][0]*w**2 + x[1][0]*w + x[2][0]
 train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
 init = tf.global_variables_intializer()
 
@@ -16,4 +16,4 @@ print(session.run(w))
 
 for i in range(1000):
   session.run(train, feed_dict={x:coefficients})
-print(session.run(w)
+print(session.run(w))
