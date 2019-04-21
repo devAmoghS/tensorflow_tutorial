9d1cf66b5b7532545af2f274b13354537d4e17ce
Amogh Singhal <devamoghs@gmail.com>
2019-04-21 18:03:20 +0530

    Create first_example.py

---
+++ first_example.py
@@ -0,0 +1,19 @@
+import numpy as np
+import tensorflow as tf
+
+coefficients = np.array([[1], [-20], [25]])
+
+w = tf.Variable([0], dtype=tf.float32)
+x = tf.placeholder(tf.float32, [3,1])
+
+cost = x[0][0]*w**2 + x[0][1]*w + x[0][2]
+train = tf.train.GradientDescentOptimizer(0.01).minimize(cost)
+init = tf.global_variables_intializer()
+
+session = tf.Session()
+session.run(init)
+print(session.run(w))
+
+for i in range(1000):
+  session.run(train)
+print(session.run(w))
